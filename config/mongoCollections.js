5ec3f23891dd9dabbd913f4e49e340278f795e08
tannerjames711 <tannerjames711@gmail.com>
2025-04-20 14:38:28 -0700

    Remove node_modules from Git tracking

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -20,4 +20,5 @@ NOTE: YOU WILL NEED TO CHANGE THE CODE BELOW TO HAVE THE COLLECTION(S) REQUIRED 
 export const cryptoRatings = getCollectionFn('cryptoRatings');
 export const financialData = getCollectionFn('financialData');
 export const newsData = getCollectionFn('newsData');
+export const offsetData = getCollectionFn('offsetData');
 
